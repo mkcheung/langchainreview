8217bd3394e0d3f6989c1006cf736b349cff8ec5
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-13 15:12:51 -0700

    demonstrate use of Tavily search tool

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -5,25 +7,24 @@ from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain.tools import tool
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
-tavily = TavilyClient()
+class Source(BaseModel):
+    """Schema for a source by the agent"""
 
-@tool
-def search(query: str) -> str:
-    """ 
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel): 
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
+
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
-agent = create_agent(model=llm, tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchaincourse!")
