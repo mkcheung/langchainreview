8d3bd273d231d979dff7a1abe2ffe372709a4102
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-13 14:33:56 -0700

    demonstrate manual call via user defined tool to tavily search

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain.tools import tool
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+tavily = TavilyClient()
 
 @tool
 def search(query: str) -> str:
@@ -16,7 +19,7 @@ def search(query: str) -> str:
         The search result
     """
     print(f"Searching for {query}")
-    return "Tokyo weather is sunny"
+    return tavily.search(query=query)
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [search]
@@ -24,7 +27,7 @@ agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchaincourse!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using lanchain in the bay area on linkedin and list their details")})
     print(result)
 
 if __name__ == "__main__":
