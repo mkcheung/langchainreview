8a597798722dc690dc19de8a6496f441badb0930
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-14 20:40:55 -0700

    scaffold for llm tool calling demonstration with react loop

---
+++ 1_agent_loop_langchain_tool_calling.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
+from langsmith import traceable
+
+MAX_ITERATIONS = 30
+MODEL = "qwen3:1.7b"
+
+# --- Tools (LangChain @tool decorator) ---
+
+@tool
+def get_product_price(product:str) -> float:
+    """Look up the price of a product in the catalog."""
+    print(f"    >> Executing get_product_price(product='{product}')")
+    prices = {"laptop":1299.99, "headphones": 149.95, "keyboard":89.50}
+    return prices.get(product, 0)
+
+@tool
+def apply_discount(price: float, discount_tier:str) -> float:
+    """Apply a discount tier to a price and return the final price. Available tiers: bronze, silver, gold. """
+    print(f"    >> Executing apply_discount(price={price}, discount_tier='{discount_tier}')")
+    discount_percentages = {"bronze": 5, "silver": 12, "gold": 23}
+    discount = discount_percentages.get(discount_tier, 0)
+
+    return round(price * (1 - discount / 100, 2))
+
+@traceable(name="LangChain Agent Loop")
+def run_agent(question:str):
+    pass
+
+if __name__ == "__main__":
+    print("Hello LangChain Agent (.bind_tools)!")
+    print()
+    result = run_agent("What is the price of a laptop after applying a gold discount?")
+
---
+++ tavilysearch_example.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
+
+class Source(BaseModel):
+    """Schema for a source by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel): 
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
+
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
+def main():
+    print("Hello from langchaincourse!")
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using lanchain in the bay area on linkedin and list their details")})
+    print(result)
+
+if __name__ == "__main__":
+    main()
