c8a6cc262d48f8f5f07c0472fab0dc3f16213f4a
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-16 12:36:14 -0700

    import loaders, text splitter, and interface with pinecone

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
+from langchain_unstructured import UnstructuredLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv()
 
