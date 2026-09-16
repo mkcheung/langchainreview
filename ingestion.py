5710980eeaac6dee84a63a6df92bebd8faaaba83
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-16 13:46:04 -0700

    add text split operations and upload to pinecone

--- ingestion.py
+++ ingestion.py
@@ -3,10 +3,23 @@ from dotenv import load_dotenv
 from langchain_unstructured import UnstructuredLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
 
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
-    print(os.environ['PINECONE_API_KEY'])
+    loader = UnstructuredLoader(file_path="/Users/marscheung/SideProjects/langchaincourse/mediumblog1.txt", chunking_strategy="basic", max_characters=1000000)
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=8)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME'])
+    print("COMPLETED")
