9b40433ce53f2bbc6ca0e2e4463820b71e102504
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-24 14:57:06 -0700

    demonstrate html text splitter with manual html string and read in from url

--- doc_loader_exercise/main.py
+++ doc_loader_exercise/main.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, WebBaseLoader, ArxivLoader, WikipediaLoader
-from langchain_classic.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter, HTMLHeaderTextSplitter
 import bs4
 import wikipedia
 
@@ -20,12 +20,64 @@ wikipedia.set_user_agent("langchaincourse-exercise/1.0 (mars.kwong.cheung@gmail.
 # print(text[1])
 
 # Demonstrate use of  character splitter
-loader=TextLoader('speech.txt')
-docs=loader.load()
-text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=100, chunk_overlap=50)
-text = text_splitter.split_documents(docs)
-print(text[0])
-print(text[1])
+# loader=TextLoader('speech.txt')
+# docs=loader.load()
+# text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=100, chunk_overlap=50)
+# text = text_splitter.split_documents(docs)
+# print(text[0])
+# print(text[1])
+
+# Demonstrate use of HTML Header Splitter
+# manual html string
+# html_string = """
+# <!DOCTYPE html>
+# <html>
+# <body>
+#     <div>
+#         <h1>Foo</h1>
+#         <p>Some intro text about Foo.</p>
+#         <div>
+#             <h2>Bar main section</h2>
+#             <p>Some intro text about Bar.</p>
+#             <h3>Bar subsection 1</h3>
+#             <p>Some text about the first subtopic of Bar.</p>
+#             <h3>Bar subsection 2</h3>
+#             <p>Some text about the second subtopic of Bar.</p>
+#         </div>
+#         <div>
+#             <h2>Baz</h2>
+#             <p>Some text about Baz</p>
+#         </div>
+#         <br>
+#         <p>Some concluding text about Foo</p>
+#     </div>
+# </body>
+# </html>
+# """
+
+# headers_to_split_on=[
+#     ("h1","Header 1"),
+#     ("h2","Header 2"),
+#     ("h3","Header 3")
+# ]
+
+# html_splitter=HTMLHeaderTextSplitter(headers_to_split_on)
+# html_header_splits=html_splitter.split_text(html_string)
+# print(html_header_splits)
+
+# Demonstrate use of HTML Header Splitter
+# data from url
+url = 'https://plato.stanford.edu/entries/goedel/'
+
+headers_to_split_on=[
+    ("h1","Header 1"),
+    ("h2","Header 2"),
+    ("h3","Header 3"),
+    ("h4","Header 4")
+]
+html_splitter=HTMLHeaderTextSplitter(headers_to_split_on)
+html_header_splits=html_splitter.split_text_from_url(url)
+print(html_header_splits)
 
 # PyPDF Loader
 # loader = PyPDFLoader('attention.pdf')
