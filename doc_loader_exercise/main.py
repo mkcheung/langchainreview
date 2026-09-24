debd48cdb2f2f8b61cbe12f23bea4b6c9fb9b9c6
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-09-24 14:28:07 -0700

    add in character text splitter demo

--- doc_loader_exercise/main.py
+++ doc_loader_exercise/main.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, WebBaseLoader, ArxivLoader, WikipediaLoader
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 import bs4
 import wikipedia
 
@@ -9,10 +10,34 @@ wikipedia.set_user_agent("langchaincourse-exercise/1.0 (mars.kwong.cheung@gmail.
 # text_documents=loader.load()
 # print(text_documents)
 
+# Demonstrate use of recusrive character splitter
+# speech: str = ""
+# with open("speech.txt") as f:
+#     speech = f.read()
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=10)
+# text = text_splitter.create_documents([speech])
+# print(text[0])
+# print(text[1])
+
+# Demonstrate use of  character splitter
+loader=TextLoader('speech.txt')
+docs=loader.load()
+text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=100, chunk_overlap=50)
+text = text_splitter.split_documents(docs)
+print(text[0])
+print(text[1])
+
 # PyPDF Loader
 # loader = PyPDFLoader('attention.pdf')
 # docs=loader.load()
 # print(docs)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+# final_documents = text_splitter.split_documents(docs)
+# # print(final_documents)
+# print(final_documents[0])
+# print(final_documents[1])
+
+
 
 # Web Based Loader
 # loader=WebBaseLoader(web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
@@ -26,10 +51,11 @@ wikipedia.set_user_agent("langchaincourse-exercise/1.0 (mars.kwong.cheung@gmail.
 # len(docs)
 # print(docs)
 
-loader = WikipediaLoader(query="Artificial Intelligence", load_max_docs=2)
-docs = loader.load()
-for i, doc in enumerate(docs):
-    print(f"--- Document {i+1} ---")
-    print(f"Source URL: {doc.metadata['source']}")
-    print(f"Title: {doc.metadata['title']}")
-    print(f"Content Preview: {doc.page_content[:200]}...\n")
+#Wikipedia Loader
+# loader = WikipediaLoader(query="Artificial Intelligence", load_max_docs=2)
+# docs = loader.load()
+# for i, doc in enumerate(docs):
+#     print(f"--- Document {i+1} ---")
+#     print(f"Source URL: {doc.metadata['source']}")
+#     print(f"Title: {doc.metadata['title']}")
+#     print(f"Content Preview: {doc.page_content[:200]}...\n")
